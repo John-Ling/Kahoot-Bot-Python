64ebdd31dd67d5238432a91e016beed983cdc5b0
John Ling <johnlingbusiness@gmail.com>
2022-09-05 12:56:43 +0800

    Light refactoring Added regex and sys library

--- main.py
+++ main.py
@@ -7,45 +7,50 @@ from selenium.webdriver.common.keys import Keys
 
 import random
 import time
-
-# Kahoot bot 2 Electric Boogaloo 
+import sys
+import re
+# Kahoot bot 2 Electric Boogaloo
 # A revamp of my kahoot bot using selenium library from september of 2019, its currently september of 2021
-# now with better programming 
+# now with better programming
+
 
 def main():
+    if len(sys.argv) < 4:
+        print("Please provide 3 arguments: Lobby ID, Bot Name, Number of Bots")
+        quit()
 
-    lobbyID = str(input("Enter lobby number: "))
+    lobbyID = str(sys.argv[1])
 
-    while len(lobbyID) != 7:
-        lobbyID = str(input("Re-enter: "))
+    if len(lobbyID) != 7 or re.search("[^A-Za-z0-9]", lobbyID): # check if id is not exclusively alphanumeric characters and right length
+        print("Lobby ID must be 7 characters containing only numbers and letters")
+        quit()
 
-    totalBotNumber = int(input("Enter number of bots: "))
+    botName = str(sys.argv[2])
+
+    totalBotNumber = int(sys.argv[3])
     numberOfDigits = len(str(totalBotNumber))
 
-    botName = str(input("Enter bot name: "))
-
     while (len(botName) + numberOfDigits > 15):
-        botName = str(input("Please use a shorter name: "))
+        print(f"Bot name should be under 13 characters")
+        quit()
 
-    host = Host(lobbyID,botName)
+    host = Host(lobbyID, botName)
 
     updatedBotNumber = totalBotNumber
-    for i in range(totalBotNumber ):
+    for i in range(totalBotNumber):
         print(f"Adding bot {i}...")
-        if i != 0:
-            host.driver.execute_script("window.open()") # open new tab
-            host.driver.switch_to.window(host.driver.window_handles[i]) 
+        if i > 0:
+            host.driver.execute_script("window.open()")  # open new tab
+            host.driver.switch_to.window(host.driver.window_handles[i])
         if i < 6:
             joinSuccessful = host.join_game(botNumber=i, delay=False)
         else:
             print("Applying delay...")
             joinSuccessful = host.join_game(botNumber=i, delay=True)
 
-        if joinSuccessful: # check if bot was able to join game
-            pass
-        else:
+        if not joinSuccessful:  # check if bot was able to join game
             updatedBotNumber -= 1
-            
+
     # update total number of bots
     totalBotNumber = updatedBotNumber
     print(f"Total number of bots is {totalBotNumber}")
@@ -57,7 +62,7 @@ def main():
     host.wait_for_url_change()
 
     # answer kahoot question of quiz until game ends
-    while host.driver.current_url != "https://kahoot.it/v2/ranking": 
+    while host.driver.current_url != "https://kahoot.it/v2/ranking":
         host.wait_for_url_change()
         print("Answering question")
         time.sleep(1)
@@ -69,13 +74,13 @@ def main():
                 host.answer_question(availableButtons)
             else:
                 # ignore this tab since the bot has failed to join
-                pass 
+                pass
             i += 1
 
         host.wait_for_url_change()
         host.wait_for_url_change()
 
-    # shutdown 
+    # shutdown
     print("Game has ended")
     input("End program: ")
     print("Terminating bots... ")
@@ -87,27 +92,29 @@ def main():
     del host
 
 
-class Bot(): # custom datatype for different traits such as their join status or if they're a leader
-    # webdriver shit is still done by Host()
+class Bot():  # custom datatype for different traits such as their join status or if they're a leader
     def __init__(self, name, score, joinSuccessful):
         self.name = name
         self.score = score
-        self.joinSuccessful = joinSuccessful 
+        self.joinSuccessful = joinSuccessful
+
 
 class Host():  # control bot for all player bots under it
     def __init__(self, lobbyID, botName):
         self.lobbyID = lobbyID
-        self.botName = botName 
+        self.botName = botName
         try:
-            self.driver = webdriver.Firefox() # webdriver to automatically control web browser
-        except AttributeError: # web driver is not in path
-            raise AttributeError("Program dun goofed. Add webdriver to path dipshit")
-        self.bots = [] # all bots currently in existence
+            # webdriver to automatically control web browser
+            self.driver = webdriver.Firefox()
+        except AttributeError:  # web driver is not in path
+            raise AttributeError("Attribute Error: Add webdriver to path")
+        self.bots = []  # all bots currently in existence
 
     def join_game(self, botNumber, delay):
         # create a bot and send it to a kahoot lobby
         # create bot
-        numberedBotName = self.botName + str(botNumber) # original bot name with its number attached to it
+        # original bot name with its number attached to it
+        numberedBotName = self.botName + str(botNumber)
         bot = Bot(numberedBotName, 0, joinSuccessful=False)
 
         joinSuccessful = False
@@ -115,7 +122,7 @@ class Host():  # control bot for all player bots under it
 
         try:
             # delay workaround since kahoot blocks bots
-            # the blocks seem to be timer based 
+            # the blocks seem to be timer based
             # refreshing the page to create a brief pause seems to be the quickest way to bypass this
             workaroundDelay = 0.5
             if delay == True:
@@ -125,12 +132,14 @@ class Host():  # control bot for all player bots under it
             # rate of bot joining reduces slightly and on average 2 bots will fail to join since the join cooldown time appears to fluctuate randomly
 
             # enter lobby ID
-            WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.ID, "game-input")))
+            WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.ID, "game-input")))
             gamePinTextBox = self.driver.find_element_by_id("game-input")
             gamePinTextBox.send_keys(self.lobbyID + Keys.ENTER)
 
             # enter name
-            WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.ID, "nickname")))
+            WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.ID, "nickname")))
             nicknameTextBox = self.driver.find_element_by_id("nickname")
             nicknameTextBox.send_keys(numberedBotName + Keys.ENTER)
 
@@ -138,34 +147,37 @@ class Host():  # control bot for all player bots under it
             bot.joinSuccessful = True
         except TimeoutException:
             print(f"Bot {botNumber} has failed to join game")
-        
+
         self.bots.append(bot)
-        return joinSuccessful 
+        return joinSuccessful
 
     def remove_options(self):
         # remove buttons that are not present in the question (such as a true or false question with only two options)
-        buttonIDPresent = ["triangle-button", "diamond-button", "circle-button", "square-button"]
-        tmp = ["triangle-button", "diamond-button", "circle-button", "square-button"]
+        buttonIDPresent = ["triangle-button",
+                           "diamond-button", "circle-button", "square-button"]
+        tmp = ["triangle-button", "diamond-button",
+               "circle-button", "square-button"]
 
         for id in buttonIDPresent:
             try:
                 self.driver.find_element_by_id(id)
             except NoSuchElementException:
                 tmp.remove(id)
-        
+
         buttonIDPresent = tmp
         return buttonIDPresent
 
     def answer_question(self, availableAnswers):
         # gets a bot to select an answer from a kahoot question
-        
+
         button = random.randint(0, len(availableAnswers) - 1)
         availableAnswers = list(availableAnswers)
         print(f"Answering")
         try:
-            WebDriverWait(self.driver, 1.5).until(EC.element_to_be_clickable((By.ID, availableAnswers[button])))
+            WebDriverWait(self.driver, 1.5).until(
+                EC.element_to_be_clickable((By.ID, availableAnswers[button])))
             button = self.driver.find_element_by_id(availableAnswers[button])
-            button.click() 
+            button.click()
         except TimeoutException:
             print("Failed to answer")
 
@@ -173,8 +185,7 @@ class Host():  # control bot for all player bots under it
         # pause until the page changes to a new url
         # i.e wait for the lobby screen to change to the block selection screen
         currentURL = self.driver.current_url
-        while currentURL == self.driver.current_url:
-            pass
+        while currentURL == self.driver.current_url: pass
 
 
 if __name__ == "__main__":
